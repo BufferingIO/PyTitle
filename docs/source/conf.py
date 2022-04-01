f234cd18c299dae6197b7513628984023eb68e30
Sina Ebrahimi <ebrahimisina78@gmail.com>
2022-04-01 22:11:35 +0430

    add mock for pydantic imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,9 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
+autodoc_mock_imports = ["pydantic"]
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
